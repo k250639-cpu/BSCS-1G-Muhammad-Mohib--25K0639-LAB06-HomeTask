f9c9921adce682c92e85bc52e8e1604b77323e0b
Muhammad Mohib <k250639@nu.edu.pk>
2025-10-05 01:15:50 -0700

    Create Question7.c

---
+++ Question7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int row = 0, prev = 0, num = 1, count = 1, temp=0;
+    printf("Enter the number of rows you want to print(fibonacci style): ");
+    scanf("%d", &row);
+    for (int i = 1; i <= row; i++) {
+        for (int j = 1; j <= count; j++) {
+            printf("%d ", num);
+            num += 1;
+        }
+        printf("\n");
+        temp = count;
+        count = count + prev;
+        prev = temp;
+        if (count == 0) 
+            count = 1;
+    }
+    return 0;
+}
